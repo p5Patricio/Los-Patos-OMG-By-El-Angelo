b8d91bfcdfff03311d8d243c0a4dfa9c41972d44
p5Patricio <pa.garciaperezvela@ugto.mx>
2024-06-03 14:59:41 -0600

    corrigiendo estilos y navbar

--- frontend/catalogo.php
+++ frontend/catalogo.php
@@ -1,4 +1,8 @@
-<?php include '../backend/config/db.php'; ?>
+<?php
+session_start();
+include '../backend/config/db.php'; // Iniciar sesión para verificar si el usuario ha iniciado sesión
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,22 +13,27 @@
 </head>
 <body>
 <div class="custom-navbar">
-        <div>
-            <h1 style="font-size: 24px;">Dinoco 🏆</h1>
-        </div>
-        <ul class="nav-tabs">
-            <li><a href="./index.php">Home</a></li>
+    <div>
+        <h1 style="font-size: 24px;">Dinoco 🏆</h1>
+    </div>
+    <ul class="nav-tabs">
+        <li><a href="./index.php">Home</a></li>
+        <?php if(isset($_SESSION['nombre'])) { ?>
+            <li><a href="./comprasyventas.php">Mis Compras y Rentas</a></li>
+            <li><a href="../backend/controllers/logout.php">Cerrar Sesión</a></li>
+        <?php } else { ?>
             <li><a href="./login.php">Log-In</a></li>
             <li><a href="./register.php">Registrarse</a></li>
-            <li><a href="./acercade.php">Acerca de Nosotros</a></li>
-            <li><a href="./contacto.php">Contacto</a></li>
-        </ul>
-    </div>
-    <h1>Catálogo de Carros</h1>
-    <?php include '../backend/controllers/mostrar_catalogo.php'; ?>
-    <footer>
-        &copy; 2024 Dinoco - Todos los derechos reservados.
-    </footer>
-    <script src="./js/acciones_catalogo.js"></script>
+        <?php } ?>
+        <li><a href="./acercade.php">Acerca de Nosotros</a></li>
+        <li><a href="./contacto.php">Contacto</a></li>
+    </ul>
+</div>
+<h1>Catálogo de Carros</h1>
+<?php include '../backend/controllers/mostrar_catalogo.php'; ?>
+<footer>
+    &copy; 2024 Dinoco - Todos los derechos reservados.
+</footer>
+<script src="./js/acciones_catalogo.js"></script>
 </body>
 </html>
